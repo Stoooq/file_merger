1dcbc2db227aa1148ac734baebf7cca70ef71b68
Milosz Glowacki <milosz.glow@gmail.com>
2025-12-04 15:07:45 +0100

    change port

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
     name: string;
   } | null>(null);
 
-  const APP_URL = import.meta.env.VITE_APP_URL || "http://ferrytwkshop.domdata.at:7996";
+  const APP_URL = import.meta.env.VITE_APP_URL || "http://ferrytworkshop.domdata.at:7996";
 
   const readFileContent = (file: File): Promise<string> => {
     return new Promise((resolve, reject) => {
